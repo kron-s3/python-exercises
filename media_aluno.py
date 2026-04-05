0478d9d005c708d8d749bce1f5221a2c0d446904
kron-s3 <thiagosilvadejes@gmail.com>
2026-04-05 08:25:26 -0300

    Update media_aluno.py
    I modified some variables that were inside and outside the scope.

--- media_aluno.py
+++ media_aluno.py
@@ -1,34 +1,34 @@
 def calcular_media(soma, qtd):
-	"""
-	Calcula a divisão entre soma e qtd.
-	Retorna o resultado dessa divisão.
-	"""
-	return soma / qtd
-	
+    """
+    Calcula a divisão entre soma e qtd.
+    Retorna o resultado dessa divisão.
+    """
+    return soma / qtd
+
 def validar_nome(msg_nome):
-	"""
-	Solicita um nome válido ao usuário.
-	Continua solicitado até ele digitar um valor válido.
-	"""
-	while True:
-		nome = input(msg_nome)
-		if nome == '':
-			print('Nome inválido.\n')
-		else:
-			return nome
-			
+    """
+    Solicita um nome válido ao usuário.
+    Continua solicitado até ele digitar um valor válido.
+    """
+    while True:
+        valor = input(msg_nome)
+        if valor == '':
+            print('Nome inválido.\n')
+        else:
+            return valor
+
 def validar_nota(msg_nota):
-	"""
-	Solicita uma nota válida ao usuário. 
-	Continua solicitado até ele digitar um valor válido.
-	"""
-	while True:
-		try: 
-			valor= float(input(msg_nota))
-		except ValueError:
-			print('Digite apenas números!\n')
-		else:
-			return valor
+    """
+    Solicita uma nota válida ao usuário. 
+    Continua solicitado até ele digitar um valor válido.
+    """
+    while True:
+        try:
+            valor = float(input(msg_nota))
+        except ValueError:
+            print('Digite apenas números!\n')
+        else:
+            return valor
 
 # Progama principal
 print('Digite um número negativo para encerrar o programa.\n')
@@ -36,21 +36,21 @@ notas = []
 nome = validar_nome('Digite o nome do aluno: ')
 
 while True:
-	nota = validar_nota(f'Digite a nota de {nome}: ')
-	if nota >= 0:
-		notas.append(nota)
-	else:
-		break
+    nota = validar_nota(f'Digite a nota de {nome}: ')
+    if nota >= 0:
+        notas.append(nota)
+    else:
+        break
 
 contador = 0
-soma = 0
+acumulador = 0
 
 for nota in notas:
-	contador += 1
-	soma += nota
+    contador += 1
+    acumulador += nota
 
 if contador == 0:
-	print('Nenhuma nota foi digitada!')
+    print('Nenhuma nota foi digitada!')
 else:
-	media = calcular_media(soma, contador)
-	print(f'\nMédia: {media:.2f}')
+    media = calcular_media(acumulador, contador)
+    print(f'\nMédia: {media:.2f}')
