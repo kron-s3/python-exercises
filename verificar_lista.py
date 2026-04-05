1f072bf985ff5d96f1dc6ee365ec4639a4b18ff8
kron-s3 <thiagosilvadejes@gmail.com>
2026-04-05 09:17:37 -0300

    Update verificar_lista.py

--- verificar_lista.py
+++ verificar_lista.py
@@ -1,9 +1,11 @@
 def verificar_lista(lista, dado):
     indices = []
+    contador = 0
     for i in range(len(lista)):
         if lista[i] == dado:
             indices.append(i)
-    return indices
+            contador += 1
+    return indices, contador
 
 # Programa principal
 numeros = [1, 2, 3, 8, 5, 9, 7, 8, 9, 10]
