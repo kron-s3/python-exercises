4efa4e552cbfd46827aae18c09bffca042ff020d
kron-s3 <thiagosilvadejes@gmail.com>
2026-04-05 08:48:18 -0300

    Update verificar_dado.py

--- verificar_dado.py
+++ verificar_dado.py
@@ -1,14 +1,10 @@
 def verificar_dado(lista, dado):
-    contador = -1
-    if dado in lista:
-        contador += 1
-        print('Dado encontrado!')
-        return f'Índice: {contador}.'
-    else:
-        return -1
+    for i in range(len(lista)):
+        if lista[i] == dado:
+            return i
+    return -1
 
 # Programa principal
-
 numeros = [8, 4, 6, 0]
 
 print(verificar_dado(numeros, 8))
