207f06432e60b949cd7305ebfc4497c3686eabeb
kron-s3 <thiagosilvadejes@gmail.com>
2026-04-11 11:20:03 -0300

    Update pedra_papel_tesoura_2.py

--- pedra_papel_tesoura_2.py
+++ pedra_papel_tesoura_2.py
@@ -1,40 +1,49 @@
+def validar_escolha(msg_escolha):
+    while True:
+        try:
+            escolha = int(input(msg_escolha))
+        except ValueError:
+            print('Valor inválido!\n')
+        else:
+            return escolha
+
+# Programa principal
 import random
 
 resultados = []
 
+jogadas = {
+    1: 'Pedra',
+    2: 'Papel',
+    3: 'Tesoura'
+}
+
+regras = {
+    1: 3,
+    2: 1,
+    3: 2,
+}
 while True:
-    jogador = int(input('Escolha sua jogada: '))
+    jogador = validar_escolha('Escolha sua jogada: ')
     if jogador == 0:
         print('Encerrando...\n')
         break
-    elif jogador == 1:
-        jogada_jogador = 'Pedra'
-    elif jogador == 2:
-        jogada_jogador = 'Papel'
-    elif jogador == 3:
-        jogada_jogador = 'Tesoura'
-    else:
+    if jogador not in jogadas:
         print('Escolha inválida!\n')
         continue
 
+    jogada_jogador = jogadas[jogador]
+
     computador = random.randint(1, 3)
-    if computador == 1:
-        jogada_computador = 'Pedra'
-    elif computador == 2:
-        jogada_computador = 'Papel'
-    else:
-        jogada_computador = 'Tesoura'
+
+    jogada_computador = jogadas[computador]
 
     print(f'Jogador: {jogada_jogador}')
     print(f'Computador: {jogada_computador}\n')
 
     if jogador == computador:
         resultado = 'Empate!'
-    elif jogador == 1 and computador == 3:
-        resultado = 'Jogador ganhou!'
-    elif jogador == 2 and computador == 1:
-        resultado = 'Jogador ganhou!'
-    elif jogador == 3 and computador == 2:
+    elif regras[jogador] == computador:
         resultado = 'Jogador ganhou!'
     else:
         resultado = 'Computador ganhou!'
